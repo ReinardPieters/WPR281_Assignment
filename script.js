7261be1c7e4c066cdeabc9154eee5f6198a2dbe4
9Quake90 <reinardpiet@gmail.com>
2024-07-23 20:18:45 +0200

    Added log in(Top right hamburger)

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const hamMenu = document.getElementById("ham-menu");
 const offScreenMenu = document.getElementById("off-screen-menu")
+const container = document.getElementById("container");
+const registerBtn = document.getElementById("register");
+const loginBtn = document.getElementById("login");
 
+registerBtn.addEventListener("click", () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener("click", () => {
+    container.classList.remove("active");
+});
 hamMenu.addEventListener("click", function(){
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
