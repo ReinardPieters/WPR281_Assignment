a38596ba3f2a9197adeee4dad8c84df2d84042e4
9Quake90 <reinardpiet@gmail.com>
2024-07-23 20:26:03 +0200

    Fixed some bugs in the js file
    So each html file has to have iets own js file in order to use event listening

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 const hamMenu = document.getElementById("ham-menu");
-const offScreenMenu = document.getElementById("off-screen-menu")
-const container = document.getElementById("container");
-const registerBtn = document.getElementById("register");
-const loginBtn = document.getElementById("login");
+const offScreenMenu = document.getElementById("off-screen-menu");
 
-registerBtn.addEventListener("click", () => {
-    container.classList.add("active");
-});
 
-loginBtn.addEventListener("click", () => {
-    container.classList.remove("active");
-});
-hamMenu.addEventListener("click", function(){
+hamMenu.addEventListener("click", () =>{
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
-})
+    return;
+});
+
