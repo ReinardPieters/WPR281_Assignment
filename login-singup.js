b077cf5e0b2a12e2662e636cd7b943668e5aff98
9Quake90 <reinardpiet@gmail.com>
2024-07-23 20:59:21 +0200

    Fixed all the stuff with login

--- login-singup.js
+++ login-singup.js
@@ -1,6 +1,12 @@
 const container = document.getElementById("container");
 const registerBtn = document.getElementById("register");
 const loginBtn = document.getElementById("login");
+const signin = document.getElementById("signin");
+
+signin.addEventListener("click",() =>{
+    const x = document.getElementById("signinemail").value;
+    const y = document.getElementById("signinpass").value;
+})
 registerBtn.addEventListener("click", () => {
     container.classList.add("active");
 });
