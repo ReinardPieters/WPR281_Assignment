f22edfeee386373fa0566110dfefbf81f8551e5b
9Quake90 <reinardpiet@gmail.com>
2024-07-24 10:31:03 +0200

    Added the course page and the return functions to the main menu for login/sign up page and the course page

---
+++ coursedetails.js
@@ -0,0 +1,13 @@
+const hamMenu = document.getElementById("ham-menu");
+const offScreenMenu = document.getElementById("off-screen-menu");
+const back = document.getElementById("back")
+hamMenu.addEventListener("click", () =>{
+    hamMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active');
+    back.classList.toggle('active');
+    if(back.disabled == true){
+        back.disabled = false
+    } else{
+        back.disabled=true
+    }
+});
