6302d3b745ea01b959dae9b87ea8d5639e087ee4
9Quake90 <reinardpiet@gmail.com>
2024-08-07 18:37:15 +0200

    SUP

--- apply.js
+++ apply.js
@@ -16,7 +16,7 @@ async function handleFormSubmit(event) {
       });
   
       if (!response.ok) {
-        throw new Error('Failed to insert course');
+        alert('You are already enrolled in that course!!');
       }
   
       alert('Course inserted successfully');
@@ -24,37 +24,6 @@ async function handleFormSubmit(event) {
       console.error(error);
     }
   }
-// Ensure the DOM is fully loaded before attaching event listeners
-document.addEventListener("DOMContentLoaded", function() {
-
-
-    // Get the necessary elements
-    const report = document.getElementById("Report"); //Name of the input
-    const dropFile = document.getElementById("Drop-File");// name of the div
-    const paragraph = document.getElementById("applyImgP");
-    const dropArea = document.getElementById("applyDropArea"); // name of the label
-
-    // Add event listener for file input change
-    // report.addEventListener("change", uploadedFile);
-
-    //     function uploadedFile() { //function when file is uploaded
-    //         dropFile.style.backgroundColor = 'rgb(153, 255, 0)';
-    //         dropFile.style.borderStyle = 'solid';
-    //         paragraph.innerHTML = "File has been uploaded";
-    //     }
-
-    // dropArea.addEventListener("dragover", function(e){ //Used for the drag and drop function
-    //     e.preventDefault();
-    // })
-
-    // dropArea.addEventListener("drop", function(e){ //Used for the drag and drop function
-    //     e.preventDefault();
-    //     report.files = e.dataTransfer.files;
-    //     uploadedFile();
-    // })
-
-});
-
 // Function to go back to home page
 function gotoHome() {
     location.href = 'index.html';
